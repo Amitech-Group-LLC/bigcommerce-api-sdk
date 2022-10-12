0a0631c266ebeef9f11cb8b4c42ede31a93f0d42
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-12 13:42:12 +0300

    feature-catalog-v3 added brand files to index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import carts from './carts'
 import cartSettings from './cart-settings'
 import cartItems from './cart-items'
+import catalogBrands from './catalog-brands'
 
 export {
   cartItems,
   carts,
   cartSettings,
+  catalogBrands
 }
