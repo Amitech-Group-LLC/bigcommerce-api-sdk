a6d0f12980ec4635176990832e907050011e90f1
dmsomov <somovdmitriy@icloud.com>
2022-10-18 11:22:53 +0300

    Customer attribute values - added get and upsert API

---
+++ src/api/customer-attribute-values.ts
@@ -0,0 +1,37 @@
+import { CustonerAttributeValuesParams, CustonerAttributeValuesData, UpsertData } from '../models/customer-attribute-value'
+import { Result } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class CustomerAttributeValues {
+  public async list(
+    params: CustonerAttributeValuesParams = {},
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<CustonerAttributeValuesData[]>> {
+    return await http
+      .get('/v3/customers/attribute-values', {...requestOptions, params})
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async upsertMany<TData extends UpsertData>(
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<CustonerAttributeValuesData[]>> {
+    return await http
+      .put('/v3/customers/attribute-values', { ...requestOptions, data })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new CustomerAttributeValues()
