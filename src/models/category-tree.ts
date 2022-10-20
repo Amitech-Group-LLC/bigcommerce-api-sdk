5f45f176747dd3641c8abe2e0a2b6b614eaf0eca
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-20 14:01:22 +0300

    feature-category-tree-v3 Fixed models for category tree

--- src/models/category-tree.ts
+++ src/models/category-tree.ts
@@ -1,17 +1,17 @@
 export type Tree = {
-  id?: number
-  name?: string
-  channels?: number[]
+  id: number
+  name: string
+  channels: number[]
 }
 
 export type CategoryTree = {
-  id?: number
-  parent_id?: number
-  depth?: number
-  path?: number[]
-  name?: string
-  is_visible?: boolean
-  children?: CategoryTree[] | null
+  id: number
+  parent_id: number
+  depth: number
+  path: number[]
+  name: string
+  is_visible: boolean
+  children: CategoryTree[] | null
 }
 
 export type CategoryTreeFilter = {
