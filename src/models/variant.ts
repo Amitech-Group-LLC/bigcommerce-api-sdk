cb0b8c9623e84b481b1833c8b78ab5282f38b6de
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-20 14:10:31 +0300

    feature-variants-v3 Fixed models for variants

--- src/models/variant.ts
+++ src/models/variant.ts
@@ -1,51 +1,51 @@
 export type VariantOptionValue = {
-  option_display_name?: string
-  label?: string
-  id?: number
-  option_id?: number
+  option_display_name: string
+  label: string
+  id: number
+  option_id: number
 }
 
 export type Variant = {
-  cost_price?: number
-  price?: number
-  sale_price?: number
-  retail_price?: number
-  weight?: number
-  width?: number
-  height?: number
-  depth?: number
-  is_free_shipping?: boolean
-  fixed_cost_shipping_price?: number
-  purchasing_disabled?: boolean
-  purchasing_disabled_message?: string
-  upc?: string
-  inventory_level?: number
-  inventory_warning_level?: number
-  bin_picking_number?: string
-  id?: number
-  product_id?: number
-  sku?: string
-  sku_id?: number
-  option_values?: VariantOptionValue[]
-  calculated_price?: number
+  cost_price: number | null
+  price: number  | null
+  sale_price: number  | null
+  retail_price: number  | null
+  weight: number | null
+  width: number | null
+  height: number | null
+  depth: number | null
+  is_free_shipping: boolean
+  fixed_cost_shipping_price: number | null
+  purchasing_disabled: boolean
+  purchasing_disabled_message: string
+  upc: string | null
+  inventory_level: number | null
+  inventory_warning_level: number | null
+  bin_picking_number: string | null
+  id: number
+  product_id: number
+  sku: string
+  sku_id: number | null
+  option_values: VariantOptionValue[]
+  calculated_price: number
 }
 
 export type VariantUpdate = {
-  cost_price?: number
-  price?: number
-  sale_price?: number
-  retail_price?: number
-  weight?: number
-  width?: number
-  height?: number
-  depth?: number
-  is_free_shipping?: boolean
-  fixed_cost_shipping_price?: number
-  purchasing_disabled?: boolean
-  purchasing_disabled_message?: string
-  upc?: string
-  inventory_level?: number
-  inventory_warning_level?: number
-  bin_picking_number?: string
-  id?: number
+  cost_price: number | null
+  price: number | null
+  sale_price: number | null
+  retail_price: number | null
+  weight: number | null
+  width: number | null
+  height: number | null
+  depth: number | null
+  is_free_shipping: boolean
+  fixed_cost_shipping_price: number | null
+  purchasing_disabled: boolean
+  purchasing_disabled_message: string
+  upc: string | null
+  inventory_level: number | null
+  inventory_warning_level: number | null
+  bin_picking_number: string | null
+  id: number
 }
