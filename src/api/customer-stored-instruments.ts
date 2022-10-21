1b06de20b2424c13cdfae3e0f2abab05bfdec9b1
dmsomov <somovdmitriy@icloud.com>
2022-10-21 11:20:26 +0300

    Customer Stored Instruments - fixed remarks

--- src/api/customer-stored-instruments.ts
+++ src/api/customer-stored-instruments.ts
@@ -1,5 +1,5 @@
 import { StoredInstrumentsData } from '../models/customer-stored-instrument'
-import { Result } from '../models/result'
+import { ListResult } from '../models/result'
 import { RequestOptions } from '../models/request-options'
 import http from '../utils/http-clients'
 import { BigcommerceApiError } from '../utils/error'
@@ -8,7 +8,7 @@ class CustomerStoredInstruments {
   public async get(
     customerId: string,
     requestOptions: RequestOptions = {}
-  ): Promise<Result<StoredInstrumentsData[]>> {
+  ): Promise<ListResult<StoredInstrumentsData[]>> {
     return await http
       .get(`/v3/customers/${customerId}/stored-instruments`, { ...requestOptions })
       .catch(ex => {
