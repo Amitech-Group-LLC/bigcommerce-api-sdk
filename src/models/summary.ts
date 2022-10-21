faa37d5ed61fdaf7bcd7d4172cc69dbb6944cf4c
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-21 12:18:57 +0300

    feature-summary-v3 Summary models returned to previous version

--- src/models/summary.ts
+++ src/models/summary.ts
@@ -1,12 +1,12 @@
 export type Summary = {
-  inventory_count: number
-  inventory_value: number
-  primary_category_id: number
-  primary_category_name: string
-  variant_count: number
-  highest_variant_price: number
-  average_variant_price: number
-  lowest_variant_price: string
-  oldest_variant_date: string
-  newest_variant_date: string
+  inventory_count?: number
+  inventory_value?: number
+  primary_category_id?: number
+  primary_category_name?: string
+  variant_count?: number
+  highest_variant_price?: number
+  average_variant_price?: number
+  lowest_variant_price?: string
+  oldest_variant_date?: string
+  newest_variant_date?: string
 }
