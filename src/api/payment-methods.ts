eea7ae5ee8db96a35ffcd45b7e73e5191b9f3205
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-24 16:19:54 +0300

    feature-payment-methods Added API and model files for payment-method v2.

---
+++ src/api/payment-methods.ts
@@ -0,0 +1,29 @@
+import { PaymentMethod } from '../models/payment-method'
+import { Result } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class PaymentMethods {
+  public async list(
+    params: {
+      limit?: number
+      page?: number
+    } = {},
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<PaymentMethod[]>> {
+    return await http
+      .get(`/v2/payments/methods`, {
+        ...requestOptions,
+        params,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new PaymentMethods()
