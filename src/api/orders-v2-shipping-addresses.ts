4edea57d1eafb70d4bd3f727a5549830a8f4e0e8
dmsomov <somovdmitriy@icloud.com>
2022-10-25 23:01:46 +0300

    Orders v2 shipping addresses - added list API

---
+++ src/api/orders-v2-shipping-addresses.ts
@@ -0,0 +1,24 @@
+import { OrdersV2ShippingAddressData, OrdersV2ShippingAddressParams } from '../models/orders-v2-shipping-address'
+import { Result } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class OrdersV2ShippingAddresses {
+  public async list<TParams extends OrdersV2ShippingAddressParams>(
+    order_id: number,
+    params: TParams,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<OrdersV2ShippingAddressData[]>> {
+    return await http
+      .get(`/v2/orders/${order_id}/shipping_addresses`, { ...requestOptions, params })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new OrdersV2ShippingAddresses()
---
+++ src/models/orders-v2-shipping-address.ts
@@ -0,0 +1,48 @@
+export type OrdersV2ShippingAddressData = {
+  id?: number
+  order_id?: number
+  items_total?: number
+  items_shipped?: number
+  base_cost?: string
+  cost_ex_tax?: string
+  cost_inc_tax?: string
+  cost_tax?: string
+  cost_tax_class_id?: number
+  base_handling_cost?: string
+  handling_cost_ex_tax?: string
+  handling_cost_inc_tax?: string
+  handling_cost_tax?: string
+  handling_cost_tax_class_id?: number
+  shipping_zone_id?: number
+  shipping_zone_name?: string
+  form_fields?: OrdersV2ShippingAddressFormField[]
+  shipping_quotes?: OrdersV2ShippingAddressShippingQuotes
+  first_name?: string
+  last_name?: string
+  company?: string
+  street_1?: string
+  street_2?: string
+  city?: string
+  state?: string
+  zip?: string
+  country?: string
+  country_iso2?: string
+  phone?: string
+  email?: string
+  shipping_method?: string
+}
+
+export type OrdersV2ShippingAddressFormField = {
+  name?: string
+  value?: string
+}
+
+export type OrdersV2ShippingAddressShippingQuotes = {
+  url?: string
+  resource?: string
+}
+
+export type OrdersV2ShippingAddressParams = {
+  limit?: number
+  page?: number
+}
