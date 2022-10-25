e4bab4768bae7b8ec0e52854a085b19613a2fbe3
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-25 16:40:55 +0300

    feature-currencies-v2 Added API and model files for Currencies.

---
+++ src/api/currencies.ts
@@ -0,0 +1,106 @@
+import { Currency } from '../models/currency'
+import { Result } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class Currencies {
+  public async list(
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<Currency[]>> {
+    return await http
+      .get(`/v2/currencies`, {
+        ...requestOptions,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async create<TData extends Currency>(
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<Currency>> {
+    return await http
+      .post(`/v2/currencies`, {
+        ...requestOptions,
+        data,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async deleteMany(
+    requestOptions: RequestOptions = {}
+  ): Promise<undefined> {
+    return await http
+      .delete(`/v2/currencies`, {
+        ...requestOptions,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async get(
+    currencyId: number,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<Currency>> {
+    return await http
+      .get(`/v2/currencies/${currencyId}`, {
+        ...requestOptions,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async update<TData extends Currency>(
+    currencyId: number,
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<Currency>> {
+    return await http
+      .put(`/v2/currencies/${currencyId}`, {
+        ...requestOptions,
+        data,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async delete(
+    currencyId: number,
+    requestOptions: RequestOptions = {}
+  ): Promise<undefined> {
+    return await http
+      .delete(`/v2/currencies/${currencyId}`, {
+        ...requestOptions,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new Currencies()
---
+++ src/models/currency.ts
@@ -0,0 +1,17 @@
+export type Currency = {
+  id?: number
+  is_transactional: boolean
+  is_default?: boolean
+  last_updated?: string
+  country_iso2?: string
+  currency_code: string
+  currency_exchange_rate: string
+  auto_update?: boolean
+  token_location: string
+  token: string
+  decimal_token: string
+  thousands_token: string
+  decimal_places: number
+  name: string
+  enabled?: boolean
+}
