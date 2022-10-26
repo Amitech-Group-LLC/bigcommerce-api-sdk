7c22c99b8924a2ed1174ab5243e797168c9680dc
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-26 15:51:15 +0300

    feature-payment-processing-v3 Added API and model files for payment processing.

---
+++ src/api/payment-processings.ts
@@ -0,0 +1,63 @@
+import { PaymentMethod, PaymentTokenPost, PaymentToken, PaymentProcessPost, PaymentProcess } from '../models/payment-processing'
+import { Result } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class PaymentProcessings {
+  public async list(
+    params: {
+        checkout_id?: string
+        order_id?: number
+    } = {},
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<PaymentMethod[]>> {
+    return await http
+      .get(`/v3/payments/methods`, {
+        ...requestOptions,
+        params,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async create<TData extends PaymentTokenPost>(
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<PaymentToken>> {
+    return await http
+      .post(`/v3/payments/access_tokens`, {
+        ...requestOptions,
+        data,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async process<TData extends PaymentProcessPost>(
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<Result<PaymentProcess>> {
+    return await http
+      .post(`/payments`, {
+        ...requestOptions,
+        data,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new PaymentProcessings()
---
+++ src/models/payment-processing.ts
@@ -0,0 +1,64 @@
+export type PaymentMethodSupportedInstrument = {
+  instrument_type:
+    | 'VISA'
+    | 'MASTERCARD'
+    | 'DISCOVER'
+    | 'AMEX'
+    | 'DINERS_CLUB'
+    | 'JCB'
+    | 'DANKORT'
+    | 'MAESTRO'
+    | 'STORED_CARD'
+  verification_value_required?: boolean
+}
+
+export type PaymentMethod = {
+  id: string
+  name: string
+  test_mode: boolean
+  type: string
+  stored_instruments: {
+    type: string
+    brand: string
+    expiry_month: number
+    expiry_year: number
+    issuer_identification_number: string
+    last_4: string
+    token: string
+    is_default: boolean
+  }
+  supported_instruments: PaymentMethodSupportedInstrument[]
+}
+
+export type PaymentTokenPost = {
+  id: number
+  is_recurring?: boolean
+}
+
+export type PaymentToken = {
+  id: string
+}
+
+export type PaymentProcessPost = {
+  payment: {
+    instrument: {
+      type?: string
+      cardholder_name?: string
+      number?: string
+      expiry_month?: number
+      expiry_year?: number
+      verification_value?: string
+      issue_month?: number
+      issue_year?: number
+      issue_number?: number
+    }
+  }
+  payment_method_id: string
+  save_instrument?: boolean
+}
+
+export type PaymentProcess = {
+  id?: string
+  transaction_type?: string
+  status?: string
+}
