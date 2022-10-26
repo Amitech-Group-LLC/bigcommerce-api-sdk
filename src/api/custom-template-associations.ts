1722e3fe0e7f350f31023df6d287bf37a1c0c80e
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-10-26 12:49:33 +0300

    feature-custom-template-association-v3 Added API and model files for Custom Template Associations.

---
+++ src/api/custom-template-associations.ts
@@ -0,0 +1,69 @@
+import {
+  CustomTemplateAssociationFilter,
+  CustomTemplateAssociation,
+  CustomTemplateAssociationPut,
+} from '../models/custom-template-association'
+import { ListResult } from '../models/result'
+import { RequestOptions } from '../models/request-options'
+import http from '../utils/http-clients'
+import { BigcommerceApiError } from '../utils/error'
+
+class CustomTemplateAssociations {
+  public async list(
+    filterParams: CustomTemplateAssociationFilter = {},
+    requestOptions: RequestOptions = {}
+  ): Promise<ListResult<CustomTemplateAssociation[]>> {
+    return await http
+      .get(`/v3/storefront/custom-template-associations`, {
+        ...requestOptions,
+        params: filterParams,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async upsertMany<TData extends CustomTemplateAssociationPut>(
+    data: TData,
+    requestOptions: RequestOptions = {}
+  ): Promise<undefined> {
+    return await http
+      .put(`/v3/storefront/custom-template-associations`, {
+        ...requestOptions,
+        data,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+
+  public async delete(
+    params: {
+      channel_id?: number
+      'entity_id:in'?: number
+      type?: 'product' | 'category' | 'brand' | 'page'
+      'id:in'?: number
+    } = {},
+    requestOptions: RequestOptions = {}
+  ): Promise<undefined> {
+    return await http
+      .delete(`/v3/storefront/custom-template-associations`, {
+        ...requestOptions,
+        params,
+      })
+      .catch(ex => {
+        if (ex.response) {
+          throw new BigcommerceApiError(ex)
+        }
+        throw ex
+      })
+  }
+}
+
+export default new CustomTemplateAssociations()
