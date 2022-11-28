66d97b26bd242b0916860b60a4669e39b27e32ff
dmsomov <somovdmitriy@icloud.com>
2022-11-28 12:06:02 +0300

    Carts - Updated models and api

--- src/management-api/models/coupon.ts
+++ src/management-api/models/coupon.ts
@@ -9,6 +9,25 @@ export enum CouponType {
 
 export type AppliedCoupon = {
   code: string
-  id: string
-  coupon_type: CouponType
+  id?: string
+  coupon_type?: CouponType
+  discounted_amount?: number
+}
+
+export type CartCoupon = {
+  coupons?: CartCouponItem
+}
+
+export type CartCouponItem = {
+  coupon_code: CartCouponCode
+}
+
+export type CartCouponCode = {
+  id?: number
+  code?: string
+  name?: string
+  discountType?: 0 | 1 | 2 | 3 | 4
+  discountAmount?: number
+  expiresDate?: number
+  totalDiscount?: number
 }
